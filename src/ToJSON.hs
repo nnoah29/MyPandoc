b96522ac02f1485c0cfb9c163a70f193b6645bab
eyramabiala <christian.abiala@epitech.eu>
2025-05-03 11:27:19 +0100

    add to json file to convert from document to json

--- src/ToJSON.hs
+++ src/ToJSON.hs
@@ -4,6 +4,58 @@ module ToJSON (
 
 import Document
 
--- Prend un Document et retourne une chaîne JSON
-toJSON :: Document -> String
+maybeField :: String -> Maybe String -> String
+maybeField _ Nothing = ""
+maybeField name (Just val) = ", \"" ++ name ++ "\": \"" ++ val ++ "\""
 
+toJSONHeader :: Header -> String
+toJSONHeader (Header tit auth date) =
+    "{\"title\": \"" ++ tit ++ "\"" ++
+    maybeField "author" auth ++
+    maybeField "date" date ++
+    "}"
+
+toJSONText :: Content -> String
+toJSONText (Text txt) = "\"" ++ txt ++ "\""
+
+toJSONItem :: Item -> String
+toJSONItem (Item contents) =
+    "{\"item\": [" ++ joinWithCommas (map toJSONContent contents) ++ "]}"
+
+toJSONContent :: Content -> String
+toJSONContent (Paragraph texts) =
+    "{\"paragraph\": [" ++ joinWithCommas (map toJSONText texts) ++ "]}"
+
+toJSONContent (CodeBlock code) =
+    "{\"codeblock\": \"" ++ code ++ "\"}"
+
+toJSONContent (Section titleOpt contents) =
+    "{\"section\": {" ++
+    (case titleOpt of
+        Just title -> "\"title\": \"" ++ title ++ "\", "
+        Nothing -> "") ++
+    "\"contents\": [" ++ joinWithCommas (map toJSONContent contents) ++ "]}}"
+
+toJSONContent (List items) =
+    "{\"list\": [" ++ joinWithCommas (map toJSONItem items) ++ "]}"
+
+toJSONContent (Link text url) =
+    "{\"link\": {\"text\": \"" ++ text ++ "\", \"url\": \"" ++ url ++ "\"}}"
+
+toJSONContent (Image alt url) =
+    "{\"image\": {\"alt\": \"" ++ alt ++ "\", \"url\": \"" ++ url ++ "\"}}"
+
+joinWithCommas :: [String] -> String
+joinWithCommas = foldr (\x acc -> if null acc then x else x ++ ", " ++ acc) ""
+
+toJSONBody :: [Content] -> String
+toJSONBody contents =
+    "[" ++ joinWithCommas (map toJSONContent contents) ++ "]"
+
+toJSON :: Document -> String
+toJSON (Document hdr contents) =
+    "{\"header\": " ++
+    toJSONHeader hdr ++
+    ", \"body\": " ++
+    toJSONBody contents ++
+    "}"
